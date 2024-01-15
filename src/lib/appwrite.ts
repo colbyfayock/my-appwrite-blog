13c8486e4f15610e8c6cb347d2451c28a865a1eb
Colby Fayock <fayock@gmail.com>
2024-01-14 22:55:04 -0500

    Step 2: Importing Database module

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,6 +1,7 @@
-import { Client } from 'appwrite';
+import { Client, Databases } from 'appwrite';
 
 export const client = new Client();
+export const databases = new Databases(client);
 
 client
   .setEndpoint(import.meta.env.PUBLIC_APPWRITE_ENDPOINT)
